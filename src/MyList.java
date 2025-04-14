2fa84d30fae105b69a0e53ae7d7edc1f636565d7
Dericeda <nurasyl.asanov@mail.ru>
2025-04-14 20:52:29 +0500

    creating Mylist interface

---
+++ src/MyList.java
@@ -0,0 +1,9 @@
+public interface MyList<E> extends Iterable<E> {
+    void add(E element);
+    void add(int index, E element);
+    E get(int index);
+    E remove(int index);
+    int size();
+    boolean isEmpty();
+    void clear();
+}
